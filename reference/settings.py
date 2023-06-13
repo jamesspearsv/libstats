76970760d153a6a85b66dce39bbd963c722479a1
James Spears <jamesspearsv@gmail.com>
2023-06-12 20:16:08 -0400

    Added homepage quick start and general style updates

--- reference/settings.py
+++ reference/settings.py
@@ -16,7 +16,7 @@ import json
 
 # Set app running mode
 # MODE == "DEV" OR "PROD"
-MODE = 'PROD'
+MODE = 'DEV'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # SECURITY WARNING: keep the secret key used in production secret!
