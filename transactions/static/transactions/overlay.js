a7614e68af4d5e4710da82cf6aed0bac69e01845
James Spears <jamesspearsv@gmail.com>
2023-07-29 22:38:24 -0400

    Code cleanup in overlay.js

--- transactions/static/transactions/overlay.js
+++ transactions/static/transactions/overlay.js
@@ -1,18 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
-    function overlayToggle () {
-        let overlay = document.querySelector("#overlay")
-        console.log("Clicked!")
-
-        if (overlay.style.display == "none") {
-            overlay.style.display = "block"
-        } else {
-            overlay.style.display = "none"
-        }
-    }
-
     let overlayButtons = document.querySelectorAll(".overlay-button")
-    
-    overlayButtons.forEach( (button) => { button.addEventListener("click", overlayToggle)})
-    console.log(overlayButtons)
+   
+    overlayButtons.forEach((button) => { button.addEventListener("click", 
+    () => {
+        console.log("Clicked!")
+        document.querySelector("#overlay").classList.toggle('disabled') 
+        })
+    })
 })
