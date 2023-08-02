dbf31d2e17a9a2fae2bd30927c91a8a06db91adb
James Spears <jamesspearsv@gmail.com>
2023-08-02 12:53:10 -0400

    Update for remote access

--- reference/settings.py
+++ reference/settings.py
@@ -25,8 +25,8 @@ if MODE == 'PROD': # Use PROD mode for running on production server
         config = json.load(config_file)
 
     DEBUG = False
-    ALLOWED_HOSTS = ['ref.libstats.work', '192.168.110.5']
-    CSRF_TRUSTED_ORIGINS = ['https://ref.libstats.work', 'http://192.168.110.5']
+    ALLOWED_HOSTS = ['app.jspears.me', '192.168.110.5']
+    CSRF_TRUSTED_ORIGINS = ['https://app.jspears.me', 'http://192.168.110.5']
     #SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
     SECRET_KEY = config['SECRET_KEY']
     #ALLOWED_IPS = config['ALLOWED_IPS']
