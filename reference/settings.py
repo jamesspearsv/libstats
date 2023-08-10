fc86aaf83ec0ad78e2fd85f12f27f08f35bff75f
James Spears <jamesspearsv@gmail.com>
2023-08-10 10:01:24 -0400

    Change to reference/settings.py

--- reference/settings.py
+++ reference/settings.py
@@ -26,7 +26,7 @@ if MODE == 'PROD': # Use PROD mode for running on production server
 
     DEBUG = False
     ALLOWED_HOSTS = ['app.libstats.work', '192.168.110.5']
-    CSRF_TRUSTED_ORIGINS = ['https://app.jspears.me', 'http://192.168.110.5']
+    CSRF_TRUSTED_ORIGINS = ['https://app.libstats.work', 'http://192.168.110.5']
     #SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
     SECRET_KEY = config['SECRET_KEY']
     #ALLOWED_IPS = config['ALLOWED_IPS']
