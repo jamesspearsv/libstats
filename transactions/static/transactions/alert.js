ad5b6b99530609f60449b30883012fb579135df9
jamesspearsv <58987727+jamesspearsv@users.noreply.github.com>
2023-10-18 08:55:30 -0400

    Code clean up in alert.js

--- transactions/static/transactions/alert.js
+++ transactions/static/transactions/alert.js
@@ -1,17 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const removeAlert = () => {
-      console.log("run!")
       const alert = document.getElementById('alert')
 
       if (alert) {
           alert.style.opacity = '0' 
           alert.addEventListener('transitionend', () => {
-              alert.remove()
+            alert.remove()
             })
         } else {
-         console.log('removed')
-        console.log('not removed!')
         return 
       }
     }
